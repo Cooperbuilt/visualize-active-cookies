11ffecd94ce484807d5106c096484b9177143d25
cooperbuilt <evanncooper@gmail.com>
2020-01-27 07:59:31 -0500

    Refactor background js

--- background.js
+++ background.js
@@ -3,7 +3,7 @@
  * @email <evcooper@wayfair.com>
  */
 
-// Store System for funsies
+/** **************** STORE SYSTEM ************************ */
 const createStore = (reducer, initialState) => {
   const store = {};
   store.state = initialState;
@@ -41,10 +41,11 @@ const reducer = (state = getInitialState(), { type, payload }) => {
         ...state,
         firstPartyOnly: false
       };
-    case "UPDATE_TAB_URL":
+    case "UPDATE_TAB":
       return {
         ...state,
-        activeTabURL: payload
+        url: payload.url,
+        id: payload.id
       };
     default:
       return state;
@@ -53,125 +54,10 @@ const reducer = (state = getInitialState(), { type, payload }) => {
 
 const store = createStore(reducer);
 
-const updateCookieCountHTML = count => {
-  const currentUrlElement = document.querySelector(".CookieManager-count");
-  currentUrlElement.innerHTML = count;
-};
-
-/**
- * @param {string} url - full url of site to gather cookies on
- * @returns {array} - returns an array of cookies
- */
-const returnAllCookies = (url, callback) => {
-  return chrome.cookies.getAll({ url: url }, callback);
-};
-
-/**
- *
- * @param {object} cookie
- * @property {string} object.name - name of the cookie
- * @property {string} object.value - value of the cookie
- */
-const createListItem = cookie => {
-  return `
-  <li class="Card">
-    <p class="Card-title">${cookie.name}</p>
-    <p class="Card-text">${cookie.value}</p>
-    <div aria-role="divider" class="Card-divider" />
-  </li>
-  `;
-};
-
-const generateCookieHtml = cookies => {
-  return cookies.sort((a, b) => {
-    if (a.name < b.name) { return -1 }
-    else {
-      return 1
-    }
-  }).map(createListItem);
-};
-
-const createCookieList = cookies => {
-  const cookieList = document.querySelector(".CookieManager-cookieList");
-  const cookieHTML = generateCookieHtml(cookies);
-  cookieList.innerHTML = cookieHTML.join("");
-};
-
-const containsWebUrl = tabInfo => {
-  return (
-    tabInfo &&
-    tabInfo.url &&
-    (tabInfo.url.includes("http") || tabInfo.url.includes("https"))
-  );
-};
-
-function displayCookies() {
-  const { firstPartyOnly } = store.getState();
-  if (firstPartyOnly) {
-    chrome.tabs.query(
-      {
-        active: true,
-        lastFocusedWindow: true
-      },
-      ([currentTab]) => {
-        if (containsWebUrl(currentTab)) {
-          console.log(currentTab)
-          returnAllCookies(
-            firstPartyOnly ? currentTab.url : null,
-            createCookieList
-          );
-          store.dispatch({ type: "UPDATE_TAB_URL", payload: currentTab.url });
-        } else {
-          chrome.cookies.onChanged.addListener(displayCookies);
-          return;
-        }
-      }
-    );
-  } else {
-    chrome.tabs.executeScript(82, {
-      code: 'console.log("hey")',
-    }, data => {
-      console.log(data)
-      if (chrome.runtime.lastError || !data || !data[0]) return;
-      const urls = data[0].map(url => url.split(/[#?]/)[0]);
-      const uniqueUrls = [...new Set(urls).values()].filter(Boolean);
-      Promise.all(
-        uniqueUrls.map(url =>
-          new Promise(resolve => {
-            chrome.cookies.getAll({ url }, resolve);
-          })
-        )
-      ).then(results => {
-        // convert the array of arrays into a deduplicated flat array of cookies
-        const cookies = [
-          ...new Map(
-            [].concat(...results)
-              .map(c => [JSON.stringify(c), c])
-          ).values()
-        ];
-        // do something with the cookies here
-        console.log(uniqueUrls, cookies);
-      });
-    });
-  }
-}
-
-// performance.getEntriesByType("resource").map(e => e.name)
+store.dispatch({});
 
 
-
-
-// TODO clean this up to be more functional
-function clearAllCookies() {
-  const cookieList = document.querySelector(".CookieManager-cookieList");
-  cookieList.innerHTML = "";
-  chrome.cookies.getAll({}, function (cookies) {
-    for (var i in cookies) {
-      removeCookie(cookies[i]);
-    }
-  });
-}
-
+/** **************** CSS/HTML MANIPULATION ************************ */
 function fadeOut(el) {
   el.style.opacity = 0;
 }
@@ -185,6 +71,83 @@ function fadeElement(element) {
     fadeOut(element)
   }, 3000);
 }
+
+const updateCookieCountHTML = count => {
+  const currentUrlElement = document.querySelector(".CookieManager-count");
+  currentUrlElement.innerHTML = count;
+};
+
+/**
+ *
+ * @param {object} cookie
+ * @property {string} object.name - name of the cookie
+ * @property {string} object.value - value of the cookie
+ */
+const createListItem = cookie => {
+  return `
+  <li class="Card">
+    <div class="Card-propertyWrapper">
+      <span class="Card-propertyName">Origin Domain:</span><p class="Card-cookieDomain">${cookie.domain}</p>
+    </div>
+    <p class="Card-cookieName">${cookie.name}</p>
+    <p class="Card-cookieValue">${cookie.value}</p>
+
+    <div aria-role="divider" class="Card-divider" />
+  </li>
+  `;
+};
+
+const createCookieList = cookies => {
+  const cookieList = document.querySelector(".CookieManager-cookieList");
+  const cookieHTML = generateCookieHtml(cookies);
+  cookieList.innerHTML = cookieHTML.join("");
+};
+
+/** **************** UTILITIES ************************ */
+/**
+ * @param {string} url - full url of site to gather cookies on
+ * @returns {array} - returns an array of cookies
+ */
+const returnAllCookies = (url, callback) => {
+  return chrome.cookies.getAll({ url: url }, callback);
+};
+
+const generateCookieHtml = cookies => {
+  return cookies.sort((a, b) => {
+    if (a.name < b.name) { return -1 }
+    else {
+      return 1
+    }
+  }).map(createListItem);
+};
+
+const containsWebUrl = tabInfo => {
+  return (
+    tabInfo &&
+    tabInfo.url &&
+    (tabInfo.url.includes("http") || tabInfo.url.includes("https"))
+  );
+};
+
+/** **************** COOKIE REMOVAL FUNCTIONS ************************ */
+
+function removeCookie(cookie) {
+  var url =
+    "http" + (cookie.secure ? "s" : "") + "://" + cookie.domain + cookie.path;
+  chrome.cookies.remove({ url: url, name: cookie.name });
+}
+
+function clearAllCookies() {
+  const cookieList = document.querySelector(".CookieManager-cookieList");
+  cookieList.innerHTML = "";
+  chrome.cookies.getAll({}, function (cookies) {
+    for (var i in cookies) {
+      removeCookie(cookies[i]);
+    }
+  });
+}
+
+/** **************** COOKIE COPYING FUNCTIONS ************************ */
 
 function copyCookies() {
   const cookies = Array.from(document.querySelectorAll(".Card"));
@@ -210,11 +173,7 @@ function copyCookies() {
   });
 }
 
-function removeCookie(cookie) {
-  var url =
-    "http" + (cookie.secure ? "s" : "") + "://" + cookie.domain + cookie.path;
-  chrome.cookies.remove({ url: url, name: cookie.name });
-}
+
 
 function setButtonHandlers() {
   const firstPartyCookiesBtn = document.getElementById("1stParty");
@@ -228,32 +187,90 @@ function setButtonHandlers() {
     store.dispatch({
       type: "FIRST_PARTY"
     });
-    const { activeTabURL } = store.getState();
-    // returnAllCookies(activeTabURL, createCookieList);
-    displayCookies()
+    updateActiveTab()
+    returnCookies()
   });
+
   allCookiesBtn.addEventListener("click", () => {
     allCookiesBtn.classList.add("is-Active");
     firstPartyCookiesBtn.classList.remove("is-Active");
     store.dispatch({
       type: "ALL_COOKIES"
     });
-    // returnAllCookies(null, createCookieList);
-    displayCookies()
+    updateActiveTab()
+    returnCookies()
   });
 
   clearCookiesButton.addEventListener("click", clearAllCookies);
   copyCookiesButton.addEventListener("click", copyCookies);
 }
 
+function returnCookies() {
+  const { url, id, firstPartyOnly } = store.getState()
+  // if first party, use active tab to get all cookies
+  if (firstPartyOnly) {
+    returnAllCookies(
+      url,
+      createCookieList
+    );
+  } else {
+    // If not first party, iterate through all requests and use their urls
+    // to get all cookies. Flatten, and create a cookie list from those
+    chrome.tabs.executeScript(id, {
+      code: 'performance.getEntriesByType("resource").map(e => e.name)',
+    }, data => {
+      if (chrome.runtime.lastError || !data || !data[0]) return;
+      const urls = data[0].map(url => url.split(/[#?]/)[0]);
+      const uniqueUrls = [...new Set(urls).values()].filter(Boolean);
+      Promise.all(
+        uniqueUrls.map(url =>
+          new Promise(resolve => {
+            chrome.cookies.getAll({ url }, resolve);
+          })
+        )
+      ).then(results => {
+        // convert the array of arrays into a deduplicated flat array of cookies
+        const cookies = [
+          ...new Map(
+            [].concat(...results)
+              .map(c => [JSON.stringify(c), c])
+          ).values()
+        ];
+        createCookieList(cookies)
+      });
+    });
+  }
+}
+
+function updateActiveTab() {
+  // Update the active tab
+  chrome.tabs.query(
+    {
+      active: true,
+      lastFocusedWindow: true
+    },
+    ([currentTab]) => {
+      if (containsWebUrl(currentTab)) {
+        store.dispatch({ type: "UPDATE_TAB", payload: { url: currentTab.url, id: currentTab.id } });
+        returnCookies()
+      } else {
+        chrome.cookies.onChanged.addListener(updateActiveTab);
+        return;
+      }
+    });
+}
+
+
+
 function afterDOMload() {
   setButtonHandlers();
-  displayCookies();
+  updateActiveTab();
 }
-store.dispatch({});
+
+/** **************** PROGRAM START ************************ */
 if (document.readyState === "loading") {
   document.addEventListener("DOMContentLoaded", afterDOMload);
 } else {
-  displayCookies();
-  chrome.cookies.onChanged.addListener(displayCookies);
+  updateActiveTab();
+  chrome.cookies.onChanged.addListener(updateActiveTab);
 }
