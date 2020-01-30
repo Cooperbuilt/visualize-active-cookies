3353ed50b525f5dc1c07e96b29294be42d4403a8
cooperbuilt <evanncooper@gmail.com>
2020-01-30 11:33:20 -0500

    Feature complete

--- background.js
+++ background.js
@@ -85,51 +85,97 @@ const updateCookieCountHTML = count => {
  * @property {string} object.value - value of the cookie
  */
 const createListItem = cookie => {
+  const dateObj = new Date(cookie.expirationDate * 1000);
+  const date = dateObj.toLocaleString().split(',')[0]
+  var url =
+    "http" + (cookie.secure ? "s" : "") + "://" + cookie.domain + cookie.path;
   return `
   <li class="Card">
-    <svg class="Trashcan" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px"
-    viewBox="0 0 25 24.8" style="enable-background:new 0 0 25 24.8;" xml:space="preserve" class="icon-Trashcan ct-delete"
-    data-ember-action="" data-ember-action-1015="1015">
-      <g class="Trashcan-open">
-        <path d="M18.7,24.4H5.9L4.9,7h14.9L18.7,24.4z M7.6,22.6H17l0.8-13.7h-11L7.6,22.6z"></path>
-        <polygon points="13.6,10.3 13.1,21.2 14.9,21.2 15.4,10.3 "></polygon>
-        <polygon points="11.5,21.2 11,10.3 9.2,10.3 9.7,21.2 "></polygon>
-        <path d="M19.1,0.7l-4.7,0.9l-0.8-1.4L8.2,1.3L8,3l-4.7,1l0.2,4.7l17.3-3.5L19.1,0.7z
+  <div class="Card-topWrapper">
+    <div class="Card-iconWrapper" data-id-delete>
+      <svg
+        class="Trashcan"
+        xmlns="http://www.w3.org/2000/svg"
+        xmlns:xlink="http://www.w3.org/1999/xlink"
+        x="0px"
+        y="0px"
+        viewBox="0 0 25 24.8"
+        style="enable-background:new 0 0 25 24.8;"
+        xml:space="preserve"
+        class="icon-Trashcan ct-delete"
+        data-ember-action=""
+        data-ember-action-1015="1015"
+      >
+        <g class="Trashcan-open">
+          <path
+            d="M18.7,24.4H5.9L4.9,7h14.9L18.7,24.4z M7.6,22.6H17l0.8-13.7h-11L7.6,22.6z"
+          ></path>
+          <polygon
+            points="13.6,10.3 13.1,21.2 14.9,21.2 15.4,10.3 "
+          ></polygon>
+          <polygon
+            points="11.5,21.2 11,10.3 9.2,10.3 9.7,21.2 "
+          ></polygon>
+          <path
+            d="M19.1,0.7l-4.7,0.9l-0.8-1.4L8.2,1.3L8,3l-4.7,1l0.2,4.7l17.3-3.5L19.1,0.7z
 
-                M8.8,1.9l4.4 -1.0 l0.5,0.8
-                L8.7,2.8z
+          M8.8,1.9l4.4 -1.0 l0.5,0.8
+          L8.7,2.8z
 
-                M5.2,6.4l0-1L18,2.8l0.3,0.9L5.2,6.4z"></path>
-      </g>
-      <g class="Trashcan-closed">
-        <path d="M6.8,8.8h11L17,22.6
-                H7.6L6.8,8.8z
-                M4.9,7l1,17.4h12.8
-                l1-17.4
-                H4.9z"></path>
-        <polygon points="13.6,10.3 13.1,21.2 14.9,21.2 15.4,10.3 "></polygon>
-        <polygon points="11.5,21.2 11,10.3 9.2,10.3 9.7,21.2 "></polygon>
-        <path d="M20.4,4h-4.8l-0.5-1.6
-                H9.5L9,4
-                H4.2
-                L3.5,8.6h17.6
-                L20.4,4z
+          M5.2,6.4l0-1L18,2.8l0.3,0.9L5.2,6.4z"
+          ></path>
+        </g>
+        <g class="Trashcan-closed">
+          <path
+            d="M6.8,8.8h11L17,22.6
+          H7.6L6.8,8.8z
+          M4.9,7l1,17.4h12.8
+          l1-17.4
+          H4.9z"
+          ></path>
+          <polygon
+            points="13.6,10.3 13.1,21.2 14.9,21.2 15.4,10.3 "
+          ></polygon>
+          <polygon
+            points="11.5,21.2 11,10.3 9.2,10.3 9.7,21.2 "
+          ></polygon>
+          <path
+            d="M20.4,4h-4.8l-0.5-1.6
+          H9.5L9,4
+          H4.2
+          L3.5,8.6h17.6
+          L20.4,4z
 
-                M9.9,3.2h4.8
-                L14.9,3.9h-5.2z
+          M9.9,3.2h4.8
+          L14.9,3.9h-5.2z
 
-                M5.6,6.7l0.2-1 h13l0.2,1
-                H5.6z"></path>
-      </g>
-    </svg>
-    <div class="Card-propertyWrapper">
-      <span class="Card-propertyName">Origin Domain:</span><p class="Card-cookieDomain">${cookie.domain}</p>
+          M5.6,6.7l0.2-1 h13l0.2,1
+          H5.6z"
+          ></path>
+        </g>
+      </svg>
     </div>
-    <p class="Card-cookieName">${cookie.name}</p>
-    <p class="Card-cookieValue">${cookie.value}</p>
-
-    <div aria-role="divider" class="Card-divider" />
-  </li>
+    <div class="Card-titleBlock">
+      <p class="Card-cookieName" data-id-name >${cookie.name}</p>
+      <p class="Card-cookieValue" data-id-value >${cookie.value}</p>
+    </div>
+  </div>
+  <div aria-role="divider" class="Card-divider" />
+  <div class="Card-propertiesGrid">
+    <div>
+      <p>domain</p>
+      <p class="Card-property" data-id-domain>${cookie.domain}</p>
+    </div>
+     <div >
+      <p>expiration</p>
+      <p class="Card-property" data-id-date>${date}</p>
+    </div>
+     <div>
+      <p>url</p>
+      <p class="Card-property" data-id-url>${url}</p>
+    </div>
+ </div>
+</li>
   `;
 };
 
@@ -137,6 +183,7 @@ const createCookieList = cookies => {
   const cookieList = document.querySelector(".CookieManager-cookieList");
   const cookieHTML = generateCookieHtml(cookies);
   cookieList.innerHTML = cookieHTML.join("");
+  setCookieRemovalHandlers()
 };
 
 /** **************** UTILITIES ************************ */
@@ -168,9 +215,7 @@ const containsWebUrl = tabInfo => {
 /** **************** COOKIE REMOVAL FUNCTIONS ************************ */
 
 function removeCookie(cookie) {
-  var url =
-    "http" + (cookie.secure ? "s" : "") + "://" + cookie.domain + cookie.path;
-  chrome.cookies.remove({ url: url, name: cookie.name });
+  chrome.cookies.remove({ url: cookie.url, name: cookie.name });
 }
 
 function clearAllCookies() {
@@ -188,9 +233,18 @@ function clearAllCookies() {
 function copyCookies() {
   const cookies = Array.from(document.querySelectorAll(".Card"));
   const cookieObject = cookies.reduce((newObject, currentCookie) => {
-    const cookieName = currentCookie.querySelector(".Card-title").innerHTML;
-    const cookieValue = currentCookie.querySelector(".Card-text").innerHTML;
-    newObject[cookieName] = cookieValue;
+    const cookieName = currentCookie.querySelector("[data-id-name]").innerHTML;
+    const cookieValue = currentCookie.querySelector("[data-id-value]").innerHTML;
+    const cookieDomain = currentCookie.querySelector("[data-id-domain]").innerHTML;
+    const cookieDate = currentCookie.querySelector("[data-id-date]").innerHTML;
+    const cookieurl = currentCookie.querySelector("[data-id-url]").innerHTML;
+
+    newObject[cookieName] = {
+      value: cookieValue,
+      domain: cookieDomain,
+      expires: cookieDate,
+      same_site: cookieurl
+    }
     return newObject;
   }, {});
   navigator.permissions.query({ name: "clipboard-write" }).then(result => {
@@ -209,7 +263,20 @@ function copyCookies() {
   });
 }
 
+function setCookieRemovalHandlers() {
+  const cards = Array.from(document.querySelectorAll(".Card"));
 
+  cards.forEach(card => {
+    const icon = card.querySelector("[data-id-delete]");
+    const cookieName = card.querySelector("[data-id-name]").innerHTML;
+    const cookieURL = card.querySelector("[data-id-url]").innerHTML;
+
+    icon.addEventListener("click", () => {
+      removeCookie({ url: cookieURL, name: cookieName });
+      returnCookies()
+    })
+  })
+}
 
 function setButtonHandlers() {
   const firstPartyCookiesBtn = document.getElementById("1stParty");
@@ -310,3 +377,5 @@ if (document.readyState === "loading") {
   updateActiveTab();
   chrome.cookies.onChanged.addListener(updateActiveTab);
 }
+
+
